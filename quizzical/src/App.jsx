db71a9ca87d9ab35c506ab5dd7850067dfd33277
abiton198 <abitonp@gmail.com>
2022-09-01 16:10:41 +0200

    .

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState, useEffect } from 'react'
 import blob from './assets/img/blob.svg'
 import Question from './Question'
@@ -5,8 +6,12 @@ import './App.css'
 import Menu from './Menu'
 import {nanoid} from 'nanoid'
 
+
 function App() {
   const [questions, setQuestions] = useState([])
+  const [checked, setChecked] = useState(true)
+  const [count, setCount] = useState(0)
+
 
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=10&category=21&encode=base64')
@@ -18,13 +23,20 @@ function App() {
     answers:shuffleArray([question.incorrect_answers])})
   }, [])
 
+  //updating the questions to new state 
   function handleClickAnswer(id, answers){
     setQuestions(questions => questions.map(() => {
       return question.id === id ? {...question, selected:answers} : question
     }))
   }
 
+  //func () to track of ques attempted true
+  function handlePlayAgain(){
+    setCount(count => count + 1)
+    setChecked(false)
+  }
 
+  //a variable that will map over a component
 const questionElement = question ? question.map(question => {
   <Question 
   key={question.id}
@@ -36,12 +48,11 @@ const questionElement = question ? question.map(question => {
 
   return (
     <div className="App">
-      <div>    
+      <div className='myBlob'>    
           <img src={blob} className="blob" alt="" />
       </div>
       < Menu />
      
-
      {questionElement}
 
       {/* <pre>{JSON.stringify(questions,null,2)}</pre>  **to see the current State(uncomment)*/} 
@@ -51,6 +62,12 @@ const questionElement = question ? question.map(question => {
       <button className="start-btn">
         check Answers
       </button>
+
+      <button className='count'>{handlePlayAgain}</button>
+
+      <div className='myBlob'>    
+          <img src={blob} className="blob" alt="" />
+      </div>
     </div>
   )
 }
--- quizzical/src/Question.jsx
+++ quizzical/src/Question.jsx
@@ -4,12 +4,44 @@ import { nanoid } from "nanoid";
 export default function Question(props) {
     let answers = props.myQuestion.answers
 
+    
     function handleClick(answer){
         if (props.myQuestion.checked){
             return question.id === id ? {...question, selected:answers} : question
         }
         props.handleClickAnswer(props.id.answer)
     }
+
+    //variable for answers to be displayed
+    const answersElement = answers.map(answer => {
+        id = null
+        if (props.myQuestion.checked){
+           if(props.myQuestion.correct == answer){
+            id = 'correct'
+           }else if(props.myQuestion.selected === answer){
+            id = 'incorrect'
+           }else{
+            id = 'not selected'
+            }
+           }   
+
+           //atob() decodes a string of data encoded using Base64
+           //button of answers
+           return (
+            <button key={nanoid} id={id} 
+            className={answers === props.myQuestion.selected? 'answer selected' : 'answer'} onClick={() => handleClick(answer)}>
+                {atob(answers)} </button>
+        )
+        }
+    )
+
+    return (
+        <div className='questions-container'>
+            <h3 className='questions'>{atob(props.myQuestion.question)}</h3>
+        {answersElement}
+        </div>
+    )
+
 
 
 
